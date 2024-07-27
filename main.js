b61bbd8abaaeae64df7eb97c08043b9104a96124
Bilal <gyazitdinovb@gmail.com>
2024-07-27 08:10:03 +0300

    Homework-JS-20

--- main.js
+++ main.js
@@ -1,64 +1,88 @@
-//! task 1
+// task 1 !
 
- let index = 0;
+// let createStudentName = {
+//     name:"Bilal",
+//     surname:"Gyazitdiniv",
+//     Age:15,
+//     faculty:"CS",
+//     university:"ALOTOO",
+//     gpa:87,
+// }
+// console.log(createStudentName);
 
- while (index <= 100) {
-   console.log(index);
-   index++;
+// task 2 !
+
+// let createStudentName = {
+//     name:"Bilal",
+//     surname:"Gyazitdiniv",
+//     Age:15,
+//     faculty:"CS",
+//     university:"ALOTOO",
+//     gpa:87,
+// }
+// console.log(delete createStudentName.faculty);
+// console.log(createStudentName);
+
+// task 3 !
+
+// let peaksoftFullPassword = {
+//     email: "admin123@gmail.com",
+//     password: "admin123",
+//     age: 16,
+//     city:"Moscow",
+// }
+// let getProperty = prompt("Enter your email...!");
+// let getPassword = prompt("Enter your password...!")
+// let GetNumber = prompt("2 + 3..?")
+// if ( getProperty in peaksoftFullPassword) {
+//     console.log(getProperty);
+// }
+//     else {
+//         alert("Извините, такого email не существует");
+//     }
+
+//      if(getPassword in peaksoftFullPassword.password) {
+//         console.log(peaksoftFullPassword.password);
+//     }
+//     else {
+//         console.log("Неверный пароль... ");
+//     }
+
+//     if (GetNumber === ) {
+        
+//     }
+
+
+
+
+
+ let peaksoftFullPassword = {
+     email: "admin123@gmail.com",
+     password: "admin123",
+     age: 16,
+     city:"Moscow",
+     sum:5,
  }
-
-//   ! task 2
-
- let counter = 2;
-
- while (counter <= 100) {
-   if (counter % 2 === 0) {
-     console.log(counter);
-   }
-   counter++;
- }
-
-//    ! task 3
-
-let count = 10;
-let sum = 1;
-
-while (count <= 0) {
-  sum = sum + count
-  count++
-}
-console.log(sum +count);
-
-// task 4
-
-let userName = "Naruto Shipuden  ";
-console.log(userName.charAt(0));
-let jamNumber = 0;
-while (jamNumber <= userName.length) {
-  if (userName.charAt(jamNumber) ==="u") {
-    console.log("the leter is in "+jamNumber+"index");
-  }  
-  jamNumber++
-}
-let Number = prompt("введите код");
-for (let kod = 0; kod < Number; kod++) {
-  console.log(kod);
+let point = 0;
+let answer = "";
+answer = prompt("Enter your email...!");
+if (answer == "admin123@gmail.com"){
+    console.log();
+    point = point + 1; 
+}else{
+    alert("Неверно ввели почту!");
 }
 
-let line = 7;
-let line2 = "";
-let line3 = "*";
-for (let ts = 0; ts < line; ts++) {
-  line2 += line3;
-  console.log(line2);
-  }
-  let wl = "7";
-  let res1 = "";
-  let res2 = "#";
-  let res3 = "0";
-
-  while (wl >= res3) {
-    res1 += res2;
-    console.log(res1);
-    res3++
-  }
+answer = prompt("Enter your password...!");
+if (answer == "admin123"){
+    console.log(answer);
+    point = point + 1;  point++ }else{
+    alert("Неверно  ввели пароль!");
+}
+answer = prompt("2+3=?")
+if (answer == "5") {
+    alert("Верно!ваша премия-" + peaksoftFullPassword.sum);
+}
+else{
+    alert("Поздравлю вы самый умный человек")
+}
