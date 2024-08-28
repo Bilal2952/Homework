6defe4fd104b215669ce0f7d7687e3c86fdd6e03
Bilal <gyazitdinovb@gmail.com>
2024-08-28 22:47:43 +0300

    Homework-JS-30

--- main.js
+++ main.js
@@ -1,83 +1,138 @@
-//  task -1
-const mainh2 = document.getElementById("main-heading");
-mainh2.style.fontSize = "60px";
-mainh2.style.color = "green";
-mainh2.textContent = "Popular products";
-console.log(mainh2);
-// task -2
-const asidechild = document.getElementsByTagName("aside");
-asidechild[0].remove();
-console.log(asidechild);
-// task-3
+const form = document.getElementById("form");
+const todoinput = document.getElementById("input");
+const buttonda = document.getElementById("buttonda");
+const ul = document.getElementById("ulid");
 
-const section = document.getElementsByClassName("section")[0];
-const h3 = document.getElementsByClassName("heading");
-section.firstElementChild.classList.remove('heading')
-console.log(section);
-// ! задача - 2
+let todos = [];
 
-let zadacha = [
-    {
-      icon: "https://www.pngall.com/wp-content/uploads/5/Instagram-Logo-PNG-Download-Image.png",
-      title: "instagram",
-      description:
-        "Instagram (рус. Инстагра́м[1]) — американская социальная сеть для обмена фотографиями и видео, основанная Кевином Систромом и Майком Кригером. В апреле 2012 года компания Facebook Inc.",
-      link: "https://www.instagram.com/themalikov.01?igsh=amE1bGRlemp6OXo2",
-    },
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
 
-    {
-      icon: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/Telegram_logo.svg/768px-Telegram_logo.svg.png",
-      title: "telegram",
-      description:
-        "Telegram (от др.-греч. τῆλε «далеко» + др.-греч. γράμμα «запись») — кроссплатформенная система мгновенного обмена сообщениями (мессенджер) с функциями обмена текстовыми, голосовыми и видеосообщениями",
-      link: "https://t.me/skillboxru.",
-    },
-  ];
+  const inputValue = todoinput.value.trim();
 
-  let taibl = document.getElementById("link");
-  zadacha.map((element) => {
-    const con = document.createElement("li");
-    con.className = "listcls";
-
-    const image = document.createElement("img");
-    image.src = element.icon;
-    image.style.width = "130px";
-    image.style.paddingLeft = "135px";
-    image.style.paddingTop = "50px";
-
-    const h1 = document.createElement("h1");
-    h1.textContent = element.title;
-    h1.style.fontSize = "45px";
-    h1.style.paddingLeft = "100px";
-    h1.style.fontFamily = " sans-serif";
-
-    const p = document.createElement("p");
-    p.textContent = element.description;
-    p.style.paddingLeft = "20px";
-    p.style.fontSize = "17px";
-    p.style.fontFamily = "sans-serif";
-
-    const button = document.createElement("button");
-    // button.textContent = element.link;
-    button.style.fontSize = "25px";
-    button.style.borderRadius = "25px";
-    button.style.marginLeft = "85px";
-    // button.style.backgroundColor = "black";
-    // button.style.color = "white";
-    button.style.border = "none";
-    button.style.cursor = "pointer";
-    button.style.padding = "12px";
-    // button.textContent = "READ  MORE";
-    button.style.width = "225px";
-    button.className = "hover";
-
-    const a = document.createElement("a");
-    a.textContent = "read more";
-    a.addEventListener("click", () => {
-      a.href = element.link;
+  if (todoinput.value.trim() === "") {
+    const divclass = document.getElementById("divclass");
+    divclass.style.display = "block";
+    const button2 = document.getElementById("button5");
+    button2.addEventListener("click", () => {
+      divclass.style.display = "none";
     });
-    button.append(a);
+  } else {
+    const newTodo = {
+      id: Date.now().toString(),
+      title: inputValue,
+      completed: false,
+    };
+    addPost(newTodo);
+    getTodos();
+  }
+  todoinput.value = "";
+});
 
-    con.append(image, h1, p, button);
-    taibl.append(con);
+const renderTodos = (todosArray = []) => {
+  ul.innerHTML = "";
+  todosArray.forEach((item) => {
+    const list = document.createElement("li");
+    const inputcheckbox = document.createElement("input");
+    inputcheckbox.className = "inputcheck";
+    inputcheckbox.type = "checkbox";
+    inputcheckbox.style.display = "block";
+    const span = document.createElement("span");
+    inputcheckbox.checked = item.completed
+    if (item.completed) {
+      span.style.textDecoration = "line-through";
+    } else {
+      span.style.textDecoration = "none";
+    }
+    span.style.textDecorationColor = "red";
+
+    inputcheckbox.addEventListener("click", () => {
+      updateTodo(item.id, item.completed);
+    });
+    const buttondelete = document.createElement("button");
+    buttondelete.textContent = "Удалить";
+
+    buttondelete.addEventListener("click", () => {
+      const divname = document.getElementById("divname");
+      divname.style.display = "block";
+
+      buttonda.addEventListener("click", () => {
+        list.remove();
+        deleteTodo(item.id);
+        inputcheckbox.style.display = "none";
+        divname.style.display = "none";
+      });
+    });
+    const buttonda = document.getElementById("buttonda");
+
+    const buttonotmen = document.getElementById("buttonotmen");
+    buttonotmen.addEventListener("click", () => {
+      divname.style.display = "none";
+	});
+
+    span.textContent = item.title;
+    list.append(span, inputcheckbox, buttondelete);
+    ul.appendChild(list);
   });
+};
+
+const BASE_URL = "https://3f5b105781d92677.mokky.dev";
+
+const addPost = async (object) => {
+  try {
+    const response = await fetch(`${BASE_URL}/todos`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(object),
+    });
+    const data = await response.json();
+    console.log("WORKING");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getTodos = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/todos`);
+    const data = await response.json();
+    renderTodos(data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getTodos();
+
+const deleteTodo = async (id) => {
+  try {
+    const response = await fetch(`${BASE_URL}/todos/${id}`, {
+      method: "DELETE",
+    });
+    getTodos();
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const updateTodo = async (id, completed) => {
+  console.log(id);
+  try {
+    const response = await fetch(`${BASE_URL}/todos/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        completed: !completed,
+      }),
+    });
+    const data = await response.json();
+    console.log(data);
+    getTodos();
+  } catch (error) {
+    console.log(error);
+  }
+};
